d1e6ed37af30918e20606137c42dcae219575ff0
Miguel <maperezromero@gmail>
2022-06-04 14:27:45 +0200

    resume ok

--- src/components/resume.js
+++ src/components/resume.js
@@ -22,22 +22,35 @@ function ResumePanel(){
     }
     
     let items=[];
-    let line=[];
-    for (let i=0;i<6;i++){
-        for (let j=1;j<=values.resume.rounds[i+1];j++){
+    if(values.resume.rounds){
+
+        let line=[];
+        for (let i=0;i<6;i++){
+            for (let j=1;j<=values.resume.rounds[i+1];j++){
+                line.push('⬛');
+            }
+            if (values.resume.rounds[i+1] === 0){
+                line.push('');
+            }
+            items.push(line.join(''));
+            line.length = 0;
+        }
+        for (let j=1;j<=values.resume.rounds['X'];j++){
             line.push('⬛');
         }
-        if (values.resume.rounds[i+1] === 0){
-            line.push('');
-        }
         items.push(line.join(''));
-        
-        line.length = 0;
+    }else{
+        for(let i = 0;i<7;i++){
+            items.push('');
+        }
+        const data = {
+            played: 0,
+            won: 0,
+            spell: 0,
+            bestSpell: 0,
+        }
     }
-    for (let j=1;j<=values.resume.rounds['X'];j++){
-        line.push('⬛');
-    }
-    items.push(line.join(''));
+
 
     return(
         <div className="resume" id="resume">
@@ -58,24 +71,25 @@ function ResumePanel(){
             </div>
             <div className="statistics">
                 <span className="values">
-                    <h2>{values.resume.played}</h2>   
+                    <h2>{values.resume.played? values.resume.played : 0}</h2>   
                     <h3>Played</h3>
                 </span>
                 <span className="values">
-                    <h2>{(values.resume.won/values.resume.played)*100}%</h2>   
+                    <h2>{values.resume.won? ((values.resume.won/values.resume.played)*100) :0}%</h2>   
                     <h3>Won</h3>
 
                 </span>
                 <span className="values">
-                    <h2>{values.resume.spell}</h2>   
+                    <h2>{values.resume.spell ? values.resume.spell : 0}</h2>   
                     <h3>Current Streak</h3>
 
                 </span>
                 <span className="values">
-                    <h2>{values.resume.bestSpell}</h2>   
+                    <h2>{values.resume.bestSpell ? values.resume.bestSpell : 0}</h2>   
                     <h3>Best Streak</h3>
                 </span>
             </div>
+            {values.resume.rounds && 
             <div className="details">
                 <h3>1: {items[0]}({Number.isNaN(values.resume.rounds[1]/values.resume.played)? 0 : Math.trunc((values.resume.rounds[1]/values.resume.played)*100)}%)</h3>
                 <h3>2: {items[1]}({Number.isNaN(values.resume.rounds[2]/values.resume.played)? 0 : Math.trunc((values.resume.rounds[2]/values.resume.played)*100)}%)</h3>
@@ -84,7 +98,7 @@ function ResumePanel(){
                 <h3>5: {items[4]}({Number.isNaN(values.resume.rounds[5]/values.resume.played)? 0 : Math.trunc((values.resume.rounds[5]/values.resume.played)*100)}%)</h3>
                 <h3>6: {items[5]}({Number.isNaN(values.resume.rounds[6]/values.resume.played)? 0 : Math.trunc((values.resume.rounds[6]/values.resume.played)*100)}%)</h3>
                 <h3>X: {items[6]}({Number.isNaN(values.resume.rounds["X"]/values.resume.played)? 0 : Math.trunc((values.resume.rounds['X']/values.resume.played)*100)}%)</h3>
-            </div>
+            </div>}
         </div>
     )
 }
